d69937d33c2e750924e28ac084b84d9c28be605b
sebastian <sebaslogo@gmail.com>
2024-07-09 17:51:24 -0500

    Added threading

--- crawler/main.py
+++ crawler/main.py
@@ -2,37 +2,42 @@ import os
 import csv
 import json
 import pickle
-from crawler.spider import Spider
 from datetime import datetime
-from urllib.request import urlopen
+from crawler.spider import Spider
+from concurrent.futures import ThreadPoolExecutor
 
+
+def crawl_site_threaded(item: dict) -> tuple[str, datetime]:
+
+    url = item['url']
+    print(f"CRAWLING DOMAIN: {url}")
+
+    spider=Spider(**item)
+
+    # Check for pickle
+    picklename = f'{spider.domain}.pickle'
+    if os.path.exists(picklename):
+        with open(picklename, 'rb') as handle:
+            spider = pickle.load(handle)
+            print(f"Restored pickle file with: {len(spider.recipes)} recipes!")
+
+    start = datetime.now()
+    spider.start()
+    elapsed = datetime.now() - start
+    print(f"Crawled {url} in: {elapsed}")
+    print(f"Found: {len(spider.seen)} links!")
+    print(f"Found: {len(spider.recipes)} recipes!")
+    print("SAVING RECIPES")
+    with open(f"./recipe_lists/{spider.domain}_recipes_{len(spider.recipes)}.csv", "w") as f:
+        writer = csv.writer(f, delimiter='\n')
+        writer.writerows([list(spider.recipes)])
+
+    return (url, elapsed)
 
 if __name__ == "__main__":
     
     with open("./websites.json") as f:
         items = json.load(f)["sources"]
 
-    for item in items:
-
-        spider=Spider(**item)
-
-        # Check for pickle
-        picklename = f'{spider.domain}.pickle'
-        if os.path.exists(picklename):
-            with open(picklename, 'rb') as handle:
-                spider = pickle.load(handle)
-                print(f"Restored pickle file with: {len(spider.recipes)} recipes!")
-
-        start = datetime.now()
-        
-        spider.start()
-
-        print(f"Elapsed: {datetime.now() - start}")
-
-        print(f"Found: {len(spider.seen)} links!")
-        print(f"Found: {len(spider.recipes)} recipes!")
-
-        print("SAVING RECIPES")
-        with open(f"./recipe_lists/{spider.domain}_recipes_{len(spider.recipes)}.csv", "w") as f:
-            writer = csv.writer(f, delimiter='\n')
-            writer.writerows([list(spider.recipes)])
+    with ThreadPoolExecutor(max_workers=16) as executor:
+        res = executor.map(crawl_site_threaded, items)
