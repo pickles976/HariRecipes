32b3790e51d1dd9f751ed704ea0379b0a64775eb
Sebastian Gomez <sebaslogo@gmail.com>
2024-11-10 15:29:35 -0600

    sorpus florpus

--- search/generate_embeddings.py
+++ search/generate_embeddings.py
@@ -16,11 +16,13 @@ print("Loading Sentence Transformer...")
 embedder = SentenceTransformer("all-MiniLM-L6-v2")
 
 # Corpus consisting of example titles
-print("Extracting titles...")
+print("Extracting recipe info...")
 corpus = [data_to_str(item) for item in recipes]
 # corpus = [item.title for item in recipes]
 print("Example Corpus")
-print(corpus[0])
+print("\n\n\n")
+print(corpus[1])
+print("\n\n\n")
 
 # Use "convert_to_tensor=True" to keep the tensors on GPU (if available)
 print("Generating corpus embeddings...")
