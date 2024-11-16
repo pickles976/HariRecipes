6eac2c4d1c0b817d560a9aa7b065bff9e3b16f3e
Sebastian Gomez <sebaslogo@gmail.com>
2024-11-16 16:39:14 -0600

    got all the templates working
    Former-commit-id: 9df1d8b28163dec415234e24f9853bdd03aa6aea

--- src/service/app.py
+++ src/service/app.py
@@ -7,16 +7,16 @@ import logging
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
 
 from fastapi import FastAPI, HTTPException
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, FileResponse
 from sentence_transformers import SentenceTransformer
 
 from src.recipe_data import RecipeData
 from src.service.db import RecipeRepoSQLite
 from src.service.search import FloatVectorSearch, BinaryVectorSearch
-from src.service.templating import home_template, query_results_template
+from src.service.templating import home_template, query_results_template, recipe_detail_template
 from src.common import load_binary_embeddings, load_full_embeddings
 
-LOG_LEVEL = os.getenv("LOG_LEVEL", default="INFO")
+LOG_LEVEL = os.getenv("LOG_LEVEL", default="DEBUG")
 FLOAT_32_SEARCH = os.getenv("FLOAT_32_SEARCH", default=0)
 BINARY_EMBEDDINGS = os.getenv("BINARY_EMBEDDINGS", default=1)
 CUDA = os.getenv("CUDA", default=0)
@@ -25,6 +25,7 @@ MAX_RESULTS = 250
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(filename='app.log', level=logging.getLevelNamesMapping()[LOG_LEVEL]) # throws exception if you use a nonsense value
+logger.addHandler(logging.StreamHandler())
 
 logger.info(f"FLOAT_32_SEARCH: {FLOAT_32_SEARCH}")
 logger.info(f"BINARY_EMBEDDINGS: {BINARY_EMBEDDINGS}")
@@ -53,15 +54,37 @@ app = FastAPI()
 async def home():
     return home_template()
 
-@app.get("/recipe_query/{query}/{num_items}", response_class=HTMLResponse)
-async def recipe_query(query, num_items: int = 20):
-    start = time.time()
-    top_k = min(num_items, MAX_RESULTS)
-    data = vector_search.query(query_string=query, top_k=top_k)
-    logger.debug(f"Got {top_k} results in {time.time() - start:.3f}s")
-    return query_results_template(data)
+@app.get('/favicon.ico', include_in_schema=False)
+async def favicon():
+    return FileResponse("./src/service/templates/favicon.ico")
 
-@app.get("/recipe/{index}")
+@app.get("/recipe_query/", response_class=HTMLResponse)
+async def recipe_query(query, num_items: str = "20"):
+    start = time.time()
+
+    # Try to parse int
+    try:
+        num_items = int(num_items)
+    except Exception:
+        num_items = 20
+
+    top_k = max(min(num_items, MAX_RESULTS), 10)
+    data = vector_search.query(query_string=query, top_k=top_k)
+    logger.debug(f"Query: {query}")
+    logger.debug(f"Num Items: {num_items}")
+    logger.debug(f"Got {top_k} results in {time.time() - start:.3f}s")
+    return query_results_template(data, query, num_items)
+
+@app.get("/recipe/", response_class=HTMLResponse)
+async def recipe(index: int):
+    recipes = recipe_repo.list_recipes([index])
+
+    if len(recipes) != 1:
+        raise HTTPException(status_code=404, detail="Recipe not found")
+
+    return recipe_detail_template(list(recipes.values())[0], index)
+
+@app.get("/recipe/json/")
 async def recipe(index: int):
     recipes = recipe_repo.list_recipes([index])
 
