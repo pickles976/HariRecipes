849ad32a98216f4b14bc385a6cb8073ff8245fd4
Sebastian Gomez <sebaslogo@gmail.com>
2024-11-16 00:01:06 -0600

    updated file downloader script

--- download_files.sh
+++ download_files.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 echo "Downloading full-sized embedding vectors..."
-curl -o ./data/recipe_embeddings.pickle "https://github.com/pickles976/HariRecipes/releases/download/data/recipe_embeddings.pickle"
+curl -L "https://github.com/pickles976/HariRecipes/releases/download/data/recipe_embeddings.pickle" > ./data/recipe_embeddings.pickle
 
 echo "Downloading full-sized embedding vectors..."
-curl -o ./data/recipe_embeddings_binary.pickle "https://github.com/pickles976/HariRecipes/releases/download/data/recipe_embeddings_binary.pickle"
+curl -L  "https://github.com/pickles976/HariRecipes/releases/download/data/recipe_embeddings_binary.pickle" > ./data/recipe_embeddings_binary.pickle
 
 echo "Downloading sqlite file..."
-curl -o ./data/recipes.sqlite.gz "https://github.com/pickles976/HariRecipes/releases/download/data/recipes.sqlite.gz"
+curl -L "https://github.com/pickles976/HariRecipes/releases/download/data/recipes.sqlite.gz" > ./data/recipes.sqlite.gz
 
 echo "Unzipping sqlite..."
 gzip -d -k ./data/recipes.sqlite.gz
+
