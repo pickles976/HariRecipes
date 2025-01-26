ed7525b880f740a1d166ad244ee0c8a2285ba76c
Sebastian <sebaslogo@gmail.com>
2025-01-26 15:12:03 -0600

    Added a small script for validating recipes with locally hosted LLM

---
+++ src/tools/cleanup/llm_cleanup.py
@@ -0,0 +1,56 @@
+import re
+import json
+import requests
+from requests import Response
+
+CHECKPOINT = 1000
+url = "http://127.0.0.1:1234/v1/chat/completions"
+headers = {"Content-Type": "application/json"}
+
+def format(data: dict) -> dict:
+    return {
+        "messages": [
+        { "role": "user", "content": json.dumps(data) }
+        ],
+        "temperature": 0.7,
+        "max_tokens": -1,
+        "stream": False
+    }
+    
+def is_valid(response: Response) -> bool:
+    """Invalid recipe will end with @N"""
+    content = response.json()["choices"][0]["message"]["content"]
+    if len(re.findall("@N", content)) == 1:
+        return False
+    return True
+
+def save_json(items: list[dict]):
+    with open(f"./data/cleaned/{len(items)}.json", "w") as f:
+        json.dump({
+            "recipes": items
+        }, f)
+
+print("Loading recipes...")
+filepath = "./data/recipes_validated.json"
+# filepath = "./test.json"
+with open(filepath, "r") as f:
+    all_recipes = json.load(f)["recipes"]
+
+print("Starting inference...")
+items = []
+for i, recipe in enumerate(all_recipes):
+
+    print(recipe["title"])
+    res = requests.post(url=url, json=format(recipe), headers=headers)
+    if res.status_code != 200:
+        raise Exception(f"Error: {res.status_code} {res.content}")
+    
+    valid = is_valid(response=res)
+    print(str(valid).upper())
+    if valid:
+        items.append(recipe)
+    
+    if i % CHECKPOINT == 0:
+        save_json(items)
+
+save_json(items)
